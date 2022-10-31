81c11f819fffa574e3fd1272d3894b9abc5020d4
Sebastian <sebcyde@hotmail.com>
2022-10-31 21:56:33 +0000

    created Redux Store

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,27 @@ import './Styles/Main.scss';
 import React, { useEffect, useState } from 'react';
 import { Routes, Route, useNavigate } from 'react-router-dom';
 import { NewsRetriever } from './API/API';
+import axios from 'axios';
 import LoadingPage from './Pages/LoadingPage';
 import Dashboard from './Pages/Dashboard';
 import Settings from './Pages/Settings';
 import Password from './Pages/Password';
+import { useDispatch } from 'react-redux';
 
 function App() {
 	const [Loading, setLoading] = useState<boolean>(true);
 	const navigate = useNavigate();
+	const dispatch = useDispatch();
 
 	const Initialise = async () => {
+		let KEY: string = 'e0dfda639a5a4f9f95e246f7a789d9a6';
+		const FrontLoad = await axios
+			.get(`https://pokeapi.co/api/v2/pokemon/ditto`)
+			.then((response) => {
+				let Data = response.data;
+				dispatch(Data);
+				console.log(Data);
+			});
 		navigate('password');
 		setLoading(false);
 	};
--- src/Redux/store.ts
+++ src/Redux/store.ts
@@ -1,7 +1,40 @@
-import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
+import {
+	configureStore,
+	ThunkAction,
+	Action,
+	createSlice,
+} from '@reduxjs/toolkit';
 
+const initialState: any[] = [];
+const initialLoading: {} = {};
+
+const InitSlice = createSlice({
+	name: 'InitialSlice',
+	initialState,
+	reducers: {
+		Initialise: (state, action) => {
+			state = action.payload;
+		},
+		Update: (state, action) => {
+			state = action.payload;
+		},
+	},
+});
+
+const LoadingSlice = createSlice({
+	name: 'Loading',
+	initialState: { value: false },
+	reducers: {
+		ToggleLoading: (state) => {
+			state.value = !state.value;
+		},
+	},
+});
+
+export const { Initialise, Update } = InitSlice.actions;
+export const { ToggleLoading } = LoadingSlice.actions;
 export const store = configureStore({
-	reducer: {},
+	reducer: { InitialSlice: InitSlice.reducer, Loading: LoadingSlice.reducer },
 });
 
 export type AppDispatch = typeof store.dispatch;
