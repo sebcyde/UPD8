8262bf8909a4c0464996257a3d15a0521a38228d
Sebastian <sebcyde@hotmail.com>
2022-10-31 20:02:45 +0000

    initial API call

--- src/API/API.ts
+++ src/API/API.ts
@@ -2,5 +2,11 @@ import axios from 'axios';
 
 export const NewsRetriever = async () => {
 	let KEY: string = 'e0dfda639a5a4f9f95e246f7a789d9a6';
-    await axios.get('', {})
+	// await axios
+	// 	.get(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${KEY}`)
+	// 	.then((response) => {
+	// 		let Data = response.data;
+	// 		console.log(Data);
+	// 		return Data;
+	// 	});
 };
--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,19 @@ import LoadingPage from './Pages/LoadingPage';
 import { Routes, Route } from 'react-router-dom';
 import Dashboard from './Pages/Dashboard';
 import Settings from './Pages/Settings';
+import { NewsRetriever } from './API/API';
 
 function App() {
 	const [Loading, setLoading] = useState<boolean>(true);
 
+	const Initialise = async () => {
+		let News = await NewsRetriever();
+		console.log(News);
+		setLoading(false);
+	};
+
 	useEffect(() => {
-		setTimeout(() => {
-			setLoading(false);
-		}, 3000);
+		Initialise();
 	}, []);
 
 	return (
@@ -20,7 +25,6 @@ function App() {
 				<LoadingPage />
 			) : (
 				<>
-					App
 					<Routes>
 						<Route path="/" element={<Dashboard />} />
 						<Route path="settings" element={<Settings />} />
